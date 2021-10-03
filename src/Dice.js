f3a8b91f4f4b057693e2e607be137e5a3c16cc34
Karine <karine.samyn@hogent.be>
2021-10-03 22:35:42 +0200

    Rename Dice

--- src/Dice.js
+++ src/Dice.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-export default function SnakeEyes({eye, onRoll}){
+export default function Dice({eye, onRoll}){
            
     return (
         <button className="snakeEye"  onClick={onRoll} disabled={eye===1}>{eye}</button>
